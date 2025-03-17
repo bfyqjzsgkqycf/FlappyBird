99863950fee32a965e823255fd4876ff7ba62a36
bfyqjzsgkqycf <19128317516@163.com>
2025-03-17 20:34:05 +0800

    feat: 添加游戏入口和主方法
    在App.java中添加了游戏入口的注释和主方法，主方法中创建了Game对象并打印空行。

--- src/app/App.java
+++ src/app/App.java
@@ -1,4 +1,13 @@
 package app;
 
+/**
+ * 游戏入口
+ * @author LU SHIJIN
+ */
+
 public class App {
+    public static void main(String[] args) {
+        new Game();
+        System.out.println();
+    }
 }
