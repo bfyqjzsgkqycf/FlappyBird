6e69daae49f49989a47c1db343ffc20b3af0f6b0
bfyqjzsgkqycf <19128317516@163.com>
2025-03-17 20:32:34 +0800

    feat: 添加音乐播放功能
    添加了音乐工具类，支持播放飞、碰撞和得分的wav音频文件。使用JDK提供的类直接解码wav文件，并通过AudioPlayer播放。

--- src/util/MusicUtil.java
+++ src/util/MusicUtil.java
@@ -1,4 +1,48 @@
 package util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+/**
+ * 音乐工具类
+ * @author LU SHIJIN
+ * wav 音频: JDK 提供的类可直接解码 mp3; JDK 没有提供支持的需要使用第三方的工具包
+ */
 public class MusicUtil {
+
+    private static AudioStream fly;
+    private static AudioStream crash;
+    private static AudioStream score;
+
+    // wav 播放
+    public static void playFly() {
+        try {
+            InputStream flyIn = new FileInputStream("resources/wav/fly.wav");
+            fly = new AudioStream(flyIn);
+        } catch (IOException ignored) {
+        }
+        AudioPlayer.player.start(fly);
+    }
+
+    public static void playCrash() {
+        try {
+            InputStream crashIn = new FileInputStream("resources/wav/crash.wav");
+            crash = new AudioStream(crashIn);
+        } catch (IOException ignored) {
+        }
+        AudioPlayer.player.start(crash);
+    }
+
+    public static void playScore() {
+        try {
+            InputStream scoreIn = new FileInputStream("resources/wav/score.wav");
+            score = new AudioStream(scoreIn);
+        } catch (IOException ignored) {
+        }
+        AudioPlayer.player.start(score);
+    }
 }
